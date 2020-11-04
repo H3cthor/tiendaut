c4caf70b08fb05e6014a6f0b78eaab7596d3847f
Hector <hectorsalbe@hotmail.com>
2020-11-04 10:23:33 -0600

    Primer commit

---
+++ assets/js/login.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+    $("#btnLogin").click(function(e){
+        e.preventDefault();
+        /*
+        var user = $("#inputUser").val().trim();
+        var pass = $("#inputPassword").val().trim();
+
+        console.log(user + " " + pass);
+        */
+        mostrarDato();
+    }); // end #btnLogin
+
+    async function mostrarDato(){
+        const datos = new FormData(document.getElementById('formulario'));
+
+        await fetch('assets/data/login.php', {
+            method: 'POST',
+            body: datos
+        })
+        .then(response => response.json())
+        .then(response => {
+            //console.log(response.dato);
+            if(response.dato=='ok'){
+                location.href="principal.html";
+            }
+            else{
+                alert("Datos incorrectos");
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    } // end mostrarDato
+});
